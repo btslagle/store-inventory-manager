5baee9f16cea32a4b79d8cc92bcc3da95921ed44
Brad Slagle <btslagle@gmail.com>
2022-04-25 09:19:08 -0500

    Add items

---
+++ index.js
@@ -0,0 +1,38 @@
+const $div = document.querySelector("div")
+const $form = document.querySelector("form")
+
+const items = [{
+    name: "Dexerity Vest",
+    sell_in: 10,
+    quality: 20
+}, {
+    name: "Aged Brie",
+    sell_in: 2,
+    quality: 0
+}, {
+    name: "Elixir of the Mongoose",
+    sell_in: 5,
+    quality: 7
+}, {
+    name: "sulfuras, Hand of Ragnaros",
+    sell_in: 0,
+    quality: 80
+}, {
+    name: "Backstage passes to a TAFKAL80ETC concert",
+    sell_in: 15,
+    quality: 20
+}, {
+    name: "Conjured Mana Cake",
+    sell_in: 3,
+    quality: 6
+}]
+
+
+function parseCategory(){}
+
+function addItem(){}
+
+function updateSellIn(){}
+
+function updateQuality(){}
+
